7667d3cb4c9b0e1431b90e5726b452fd7c93b6fb
Thiago Amendola <amendolathiago@gmail.com>
2026-07-31 12:24:39 -0700

    Add V1 of dialog box

--- include/dialog_manager.h
+++ include/dialog_manager.h
@@ -15,11 +15,16 @@ public:
 
 private:
     static constexpr int CHAR_SPEED = 1;
+    static constexpr int DIALOG_COLS = 12;
+    static constexpr int DIALOG_ROWS = 3;
+    static constexpr int DIALOG_TILE_SPACING = 15;
+    static constexpr int DIALOG_START_X = -65;
+    static constexpr int DIALOG_START_Y = 38;
     static constexpr int SUBTITLE_X = 30;
-    static constexpr int SUBTITLE_Y = 56;
+    static constexpr int SUBTITLE_Y = 52;
 
     bn::sprite_text_generator _subtitle_text_generator;
-    bn::vector<bn::sprite_ptr, 4> _dialog_sprites;
+    bn::vector<bn::sprite_ptr, DIALOG_COLS * DIALOG_ROWS> _dialog_sprites;
     bn::vector<bn::sprite_ptr, 40> _subtitle_text_sprites;
     bn::string<64> _subtitle_text;
     int _subtitle_character_index = 0;
--- src/dialog_manager.cpp
+++ src/dialog_manager.cpp
@@ -1,6 +1,7 @@
 #include "dialog_manager.h"
 
-#include "bn_sprite_items_dialog.h"
+#include "bn_sprite_builder.h"
+#include "bn_sprite_items_dialog_bg.h"
 #include "bn_utility.h"
 
 #include "common_variable_8x16_sprite_font.h"
@@ -8,20 +9,6 @@
 namespace
 {
     constexpr char SUBTITLE_TEXT[] = "This is my subtitle";
-
-    struct dialog_sprite_entry
-    {
-        bn::fixed x;
-        bn::fixed y;
-        int graphics_index;
-    };
-
-    static const dialog_sprite_entry dialog_entries[] = {
-        // { -76, 70, 0 },
-        // { -12, 70, 1 },
-        // {  52, 70, 2 },
-        // {  116, 70, 3 },
-    };
 }
 
 dialog_manager::dialog_manager() :
@@ -31,12 +18,21 @@ dialog_manager::dialog_manager() :
     _subtitle_text_generator.set_z_order(-1);
     _subtitle_text_generator.set_center_alignment();
 
-    for (const dialog_sprite_entry& entry : dialog_entries)
+    bn::sprite_tiles_ptr dialog_tiles = bn::sprite_items::dialog_bg.tiles_item().create_tiles();
+    bn::sprite_palette_ptr dialog_palette = bn::sprite_items::dialog_bg.palette_item().create_palette();
+    bn::sprite_builder builder(bn::sprite_items::dialog_bg.shape_size(), dialog_tiles, dialog_palette);
+    builder.set_bg_priority(0);
+    builder.set_z_order(0);
+
+    for (int row = 0; row < DIALOG_ROWS; ++row)
     {
-        bn::sprite_ptr sprite = bn::sprite_items::dialog.create_sprite(entry.x, entry.y, entry.graphics_index);
-        sprite.set_bg_priority(0);
-        sprite.set_z_order(0);
-        _dialog_sprites.push_back(bn::move(sprite));
+        builder.set_y(DIALOG_START_Y + row * DIALOG_TILE_SPACING);
+
+        for (int col = 0; col < DIALOG_COLS; ++col)
+        {
+            builder.set_x(DIALOG_START_X + col * DIALOG_TILE_SPACING);
+            _dialog_sprites.push_back(builder.build());
+        }
     }
 }
 
